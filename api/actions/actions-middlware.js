0cad05997ba07dda0c83d530b9db8f26186bea33
Percouple <calebsevan@gmail.com>
2024-03-07 14:56:21 -0600

    Completed Tests

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,18 @@
 // add middlewares here related to actions
+const Actions = require('./actions-model')
+
+function validateAction (req, res, next) {
+    console.log("Validating action object")
+    // if (req.method === 'PUT' && !Object.keys(req.body).includes(('completed'))) {
+    //     return res.status(400).json({ message: "Completed status required for updating a action" });
+    // }
+    if (!req.body.notes || !req.body.description || !req.body.project_id) {
+        return res.status(400).json({ message: `Name and description are required for ${req.method === 'PUT' ? 'updating a ' : 'a '} action` });
+    }
+    // If validation passes, call next() to proceed to the next middleware
+    next();
+}
+
+module.exports = {
+    validateAction: validateAction
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -2,25 +2,72 @@
 const express = require('express');
 const router = express.Router();
 const Actions = require('./actions-model');
+const { validateAction } = require('./actions-middlware');
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res, next) => {
     console.log("ROUTING THRU ACTIONS")
+    await Actions.get()
+    .then(result => {
+        res.status(200).json(result);
+    })
+    .catch(next)
 })
 
-router.get('/:id', async (req, res) => {
-
-})
-
-router.post('/', async (req, res) => {
-
-})
-
-router.put('/:id', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
+    const id = req.params.id;
     
+    await Actions.get(id)
+        .then(result => {
+            if (!result) {
+                const error = new Error('No project with given id');
+                error.status = 404;
+                throw error;
+            }
+            res.status(200).json(result);
+        })
+        .catch(next)
 })
 
-router.delete('/', async (req, res) => {
+router.post('/', validateAction, async (req, res, next) => {
+    await Actions.insert(req.body)
+    .then(result => {
+        console.log(result)
+        res.status(200).json(result);
+    })
+    .catch(next)
+})
 
+router.put('/:id', validateAction, async (req, res, next) => {
+    const id = req.params.id;
+    await Actions.update(id, req.body)
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch(next)
+})
+
+router.delete('/:id', async (req, res, next) => {
+    const id = req.params.id;
+    await Actions.remove(id)
+        .then((result) => {
+            if (!result) {
+                const error = new Error('No project with given id');
+                error.status = 404;
+                throw error;
+            }
+            res.status(200).json(req.body);
+        })
+        .catch(next)
+})
+
+// Global error handling middleware
+router.use((err, req, res, next) => {
+    console.log(`Error status received: ${err.status}`)
+    res.status(err.status || 500).json({
+        customMessage: "Running through general error handler",
+        message: err.message,
+        stack: err.stack,
+    })
 })
 
 module.exports = router;
